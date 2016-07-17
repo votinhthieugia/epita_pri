5388cf9fffd13ecb105aef74fa88d690b2382010
Hoang Anh Doan <hoanganhdoan2003@yahoo.com>
2016-07-17 22:05:32 +0200

    Added view controller.

--- RackRepresentatorJava/src/fr/epita/pri/rackrepresentator/main/Main.java
+++ RackRepresentatorJava/src/fr/epita/pri/rackrepresentator/main/Main.java
@@ -1,16 +1,18 @@
 package fr.epita.pri.rackrepresentator.main;
 
-import fr.epita.pri.rackrepresentator.view.Window;
+import fr.epita.pri.rackrepresentator.view.ViewController;
 
 public class Main {
 	
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-            	new Window("Rack Representator");
-            }
-        });
+//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+////            	new Window("Rack Representator");
+////            	new LoginView("Rack Representator - Login");
+//            }
+//        });
+    	ViewController.Instance().start();
     }
 }
--- RackRepresentatorJava/src/fr/epita/pri/rackrepresentator/view/Window.java
+++ RackRepresentatorJava/src/fr/epita/pri/rackrepresentator/view/Window.java
@@ -2,31 +2,44 @@ package fr.epita.pri.rackrepresentator.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
 
 import fr.epita.pri.rackrepresentator.main.Console;
+import fr.epita.pri.rackrepresentator.main.SessionController;
 
-public class Window extends JFrame {
+public class Window extends BaseView implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	
 	private BaseFrame[] frames;
+	private JMenuBar menuBar;
+	private JMenu menu;
+	private JMenuItem menuItem;
 
-	public Window(String name){
-		super(name);
+	public Window(String name, IViewController controller) {
+		super(name, controller);
+		setViewId(ViewId.Main);
 		
-        frames = new BaseFrame[FrameId.TOTAL];
+		frames = new BaseFrame[FrameId.TOTAL];
         
         BottomFrame bottomFrame = new BottomFrame(this);
         frames[FrameId.BOTTOM] = bottomFrame;
         getContentPane().setLayout(new BorderLayout(0, 0));
-        SideFrame sideFrame = new SideFrame(this);
-        sideFrame.setBackground(Color.DARK_GRAY);
-        frames[FrameId.SIDE] = sideFrame;
-		
-        getContentPane().add(sideFrame, BorderLayout.WEST);
         getContentPane().add(bottomFrame, BorderLayout.SOUTH);
+        
+//        SideFrame sideFrame = new SideFrame(this);
+//        sideFrame.setBackground(Color.DARK_GRAY);
+//        frames[FrameId.SIDE] = sideFrame;
+//        getContentPane().add(sideFrame, BorderLayout.WEST);
         
         Console.Instance().setConsoleComponent(bottomFrame.getTxtAreaConsole());
         
@@ -39,9 +52,43 @@ public class Window extends JFrame {
         
         getContentPane().add(scroll, BorderLayout.CENTER);
         
+        createMenu();
+        
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);        
         this.setVisible(true);
+	}
+	
+	private void createMenu() {
+        menuBar = new JMenuBar();
+		menu = new JMenu("Rack Representator");
+		menuItem = new JMenuItem("About Us");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+		menuItem = new JMenuItem("Setting");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+		menuBar.add(menu);
+		
+		menu = new JMenu("File");
+		menuItem = new JMenuItem("Open");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+		menuBar.add(menu);
+		
+		menu = new JMenu("User");
+		menuItem = new JMenuItem("Logout");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+		menuItem = new JMenuItem("Profile");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+		menuItem = new JMenuItem("Manage");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+		menuBar.add(menu);
+		
+		setJMenuBar(menuBar);
 	}
 	
 	public BaseFrame getFrame(int frameId) {
@@ -49,5 +96,37 @@ public class Window extends JFrame {
 			return frames[frameId];
 		}
 		return null;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() instanceof JMenuItem) {
+			JMenuItem menuItem = (JMenuItem)e.getSource();
+			switch (menuItem.getText()) {
+			case "About Us":
+				break;
+			case "Setting":
+				break;
+			case "Open":
+				final JFileChooser fc = new JFileChooser();
+				int response = fc.showOpenDialog(this);
+				if (response == JFileChooser.APPROVE_OPTION) {
+					MainFrame mainFrame = (MainFrame)getFrame(FrameId.MAIN);
+					mainFrame.loadFromFile(fc.getSelectedFile().getAbsolutePath());
+				}
+				break;
+			case "Logout":
+				SessionController.destroy();
+				controller.restart();
+				break;
+			case "Profile":
+				break;
+			case "Manage":
+				controller.show(ViewId.ManageUser);
+				break;
+			default:
+				break;
+			}
+		}
 	}
 }
